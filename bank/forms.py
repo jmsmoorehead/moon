53158964230097d882ecf2925e7cfe29fa7583ed
msaizar <saizar.martin@gmail.com>
2013-09-16 09:13:28 -0300

    added API for bank and contact app, updated requirements.txt

--- bank/forms.py
+++ bank/forms.py
@@ -4,14 +4,20 @@ from .models import Transfer
 
 class DepositForm(forms.ModelForm):
 
+
     class Meta:
         model = Transfer
         fields = ('contact', 'amount',)
+        
+        
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        if amount > 0:
+            amount = amount * -1
+        return amount    
 
     def save(self, force_insert=False, force_update=False, commit=True):
         obj = super(DepositForm, self).save(commit=False)
-        if obj.amount > 0:
-            obj.amount = obj.amount * -1
         obj.transfer_type = 'D'
         obj.save()
         return obj
@@ -21,11 +27,16 @@ class WithdrawalForm(forms.ModelForm):
     class Meta:
         model = Transfer
         fields = ('contact', 'amount',)
+    
+    
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        if amount < 0:
+            amount = amount * -1
+        return amount
         
     def save(self, force_insert=False, force_update=False, commit=True):
         obj = super(WithdrawalForm, self).save(commit=False)
         obj.transfer_type = 'W'
-        if obj.amount < 0:
-            obj.amount = obj.amount * -1
         obj.save()
         return obj
